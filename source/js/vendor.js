4902f5ae1e982586bb4f7f07e8e23e4c8e31113c
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2022-05-31 07:47:58 +0300

    Проводит эксперименты со слайдером

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,26 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import Swiper from './vendor/swiper';
+// import './vendor/swiper-old';
+
+const trainersSwiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.trainers__swiper-next-btn',
+    prevEl: '.trainers__swiper-prev-btn',
+  },
+  loop: true,
+  // watchOverflow: true,
+  spaceBetween: 40,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+export {trainersSwiper};
